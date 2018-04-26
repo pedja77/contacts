2ac04d3a93e3887811734c515cfa91a051b8cb1a
pedja77 <pedja77@stcable.net>
2018-04-26 21:04:19 +0200

    Added bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button class="button button-primary">Hello bootstrap</button>
   </div>
 </template>
 
